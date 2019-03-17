1d8938c0fb6cc65e5e7b888565230aa7bc470cde
satya veera <vsatyav007@gmail.com>
2019-03-17 22:50:35 +0530

    Added some more regular expressions for removal of temperature
    Added some more regular expressions for removal of temperature and
    replaced &amp with and 's with is

--- PreProcessingTweets.py
+++ PreProcessingTweets.py
@@ -25,13 +25,13 @@ contraction_dict = {"ain't": "is not", "aren't": "are not","can't": "cannot", "'
                     "shan't": "shall not", "sha'n't": "shall not", "shan't've": "shall not have", "she'd": "she would", "she'd've": "she would have", "she'll": "she will", "she'll've": "she will have", 
                     "she's": "she is", "should've": "should have", "shouldn't": "should not", "shouldn't've": "should not have", "so've": "so have","so's": "so as", "this's": "this is","that'd": "that would",
                     "that'd've": "that would have", "that's": "that is", "there'd": "there would", "there'd've": "there would have", "there's": "there is", "here's": "here is","they'd": "they would", 
-                    "they'd've": "they would have", "they'll": "they will", "they'll've": "they will have", "they're": "they are", "they've": "they have", "to've": "to have", "wasn't": "was not",
+                    "they'd've": "they would have", "they'll": "they will", "they'll've": "they will have", "they're": "they are", "they've": "they have", "to've": "to have","today's": "today is","tomorrow's":"tomorrow is", "wasn't": "was not",
                     "we'd": "we would", "we'd've": "we would have", "we'll": "we will", "we'll've": "we will have", "we're": "we are", "we've": "we have", "weren't": "were not", "what'll": "what will",
                     "what'll've": "what will have", "what're": "what are",  "what's": "what is", "what've": "what have", "when's": "when is", "when've": "when have", "where'd": "where did", 
                     "where's": "where is", "where've": "where have", "who'll": "who will", "who'll've": "who will have", "who's": "who is", "who've": "who have", "why's": "why is", 
                     "why've": "why have", "will've": "will have", "won't": "will not", "won't've": "will not have", "would've": "would have", "wouldn't": "would not", 
                     "wouldn't've": "would not have", "y'all": "you all", "y'all'd": "you all would","y'all'd've": "you all would have","y'all're": "you all are","y'all've": "you all have",
-                    "you'd": "you would", "you'd've": "you would have", "you'll": "you will", "you'll've": "you will have", "you're": "you are", "you've": "you have"}
+                    "you'd": "you would", "you'd've": "you would have", "you'll": "you will", "you'll've": "you will have", "you're": "you are", "you've": "you have","&amp;":"and"}
 
 def _get_contractions(contraction_dict):
     contraction_re = re.compile('(%s)' % '|'.join(contraction_dict.keys()))
@@ -71,21 +71,27 @@ def preprocess():
                         #remove urls
                         x = re.sub(r'https?:\/\/.*,', ',', x)
                         #remove datetime
+                        x=re.sub(r'\d+:\d+[ap]m:? +([ce]dt)?',' ',x)
+                        #x=re.sub(r'cdt +',' ',x)
                         x=re.sub(r'\d{4}-\d{2}-\d{2} +\d{2}:\d{2}:\d{2},', '', x)
+                        #remove temperature
+                        x=re.sub(r'\d+\/\d+°[cf]',' ',x)
                         #remove numbers
                         x=re.sub(r'[0-9]*,?','',x)
                         #remove floating point numbers
                         x=re.sub(r'-?[0-9]*\.[0-9]*,?',' ',x)
                         #replace contraction with acutal words
                         x=replace_contractions(x)
+                        x=re.sub(r'\'s',' is',x)
                         #remove hashtags and user tags
-                        x=re.sub(r'[#@][a-zA-Z0-9]* +',' ',x)
+                        x=re.sub(r'[#@][a-zA-Z0-9\.]* +',' ',x)
                         #remove special characters
                         x=re.sub(r'[:-?;&)(!"*%_+$~/\[\]]','',x)
                         #remove emoji's
                         x=emoji.get_emoji_regexp().sub(u'', x)
                         #removing non word characters
                         x=re.sub(r'[^\w]', ' ', x)
+                        x=re.sub(r'[^a-zA-Z0-9]',' ',x)
                         #removing white space characters
                         x=x.strip()
                         #removing additional white spaces in between sentences

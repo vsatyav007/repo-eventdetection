3e0677d465eb3b237918cf2299219d892847c91b
Satya Veera Vemisetti <vsatyav007@gmail.com>
2019-03-28 22:28:56 +0530

    removed duplicate tuples without considering order
    added logic to remove duplicate tuples without considering order

--- GetWordPairs.py
+++ GetWordPairs.py
@@ -20,28 +20,29 @@ def getwordpairs():
     stop_words=set(stopwords.words('english'))
     #WordPairs List
     wordpairs_global =[]
-    pdatawd=os.getcwd()+'/processeddata/'
-    for subdir, dirs, files in os.walk(pdatawd):  
-        for file in files:
-            filepath = subdir + os.sep + file
-            if filepath.endswith('.txt'):
-                f1=open(filepath,'r',encoding='utf-8')
-                for line in f1:
-                    wordslist=[]
-                    words=word_tokenize(line)
-                    for word in words:
-                        if len(word)>1:
-                            if not word in stop_words:
-                                if not word in wordslist:
-                                    wordslist.append(word)
-                    wordpairs=itertools.combinations(wordslist,2)
-                    [wordpairs_global.append(t) for t in wordpairs]
-                f1.close()
+    pdatawd=os.getcwd()+'/processeddata'
+    for subdir, dirs, files in os.walk(pdatawd):
+        if not dirs :
+            for file in files:
+                filepath = subdir + os.sep + file
+                if filepath.endswith('.txt'):
+                    f1=open(filepath,'r',encoding='utf-8')
+                    for line in f1:
+                        wordslist=[]
+                        words=word_tokenize(line)
+                        for word in words:
+                            if len(word)>1:
+                                if not word in stop_words:
+                                    if not word in wordslist:
+                                        wordslist.append(word)
+                        wordpairs=itertools.combinations(wordslist,2)
+                        [wordpairs_global.append(t) for t in wordpairs]
+                    f1.close()
     
     f2=open(wordpairspath,'w+',encoding='utf-8')
-    f2.write(str(list(set(wordpairs_global))))
+    f2.write(str(list(set(tuple(sorted(wordpair)) for wordpair in wordpairs_global))))
     print(len(wordpairs_global))
-    print(len(set(wordpairs_global)))
+    print(len(set(tuple(sorted(wordpair)) for wordpair in wordpairs_global)))
     f2.close()
     
 def wordpairstodf():
